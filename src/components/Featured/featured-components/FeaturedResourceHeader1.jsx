1c1006c3ca76a0621344cd7f799e33c5216b3b6f
khalid09dev <mdkhalidhossen@hotmail.com>
2025-03-28 14:08:33 +0600

    feat: added featuredresourceheader2 component

---
+++ src/components/Featured/featured-components/FeaturedResourceHeader1.jsx
@@ -0,0 +1,8 @@
+export const FeaturedResourceHeader1 = () => {
+    return (
+        <>
+            <h1 className="text-[#1B2530] dark:text-white md:px-0 px-1 text-[36px] font-medium">Featured Resources and Products</h1>
+            <p className="text-[#70808A] md:px-0 px-1 inter text-base font-medium pb-7">Trending, popular and featured resources, loved by the users most</p>
+        </>
+    );
+};
---
+++ src/components/Featured/featured-components/FeaturedResourceHeader2.jsx
@@ -0,0 +1,8 @@
+export const FeaturedResourceHeader2 = () => {
+    return (
+        <>
+            <h1 className="text-[#1B2530] dark:text-white text-[36px] font-medium leading-10 pb-3">Free Tools, Products and Resources for <br /> Developers and Designers</h1>
+            <p className="text-[#70808A] dark:text-white inter text-base font-medium pb-7">You&apos;re exploring all available resources and products from all categories. Massive collection of the latest web development tools, design resources, and more. If you would like to sort them by category choose resource type from sidebar also you can sort by popular upvotes from right side. If you find something interesting feel free to submit and upvote resources you like most. All the resources handpicked for developers and designers to improve workflow.</p>
+        </>
+    );
+};

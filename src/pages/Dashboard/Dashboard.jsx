0ea3eaa89902f64060634ad36c068c35782b994c
khalid09dev <mdkhalidhossen@hotmail.com>
2025-04-15 17:18:03 +0600

    refactor: moved logic into a custom hook

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,42 +1,18 @@
-import { useContext, useEffect, useState } from 'react';
-import { AuthContext } from '../../providers/AuthProvider';
-import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Navbar from "../../components/Navbar/Navbar";
-import Footer from "../../components/Footer/Footer";
-import useAdmin from "../../hooks/useAdmin.js";
-import useModerator from "../../hooks/useModerator.js";
 import Sidebar from "./Sidebar.jsx";
+import useDashboard from '../../hooks/useDashboard.js';
 
 
 const Dashboard = () => {
-    const { user, logOut } = useContext(AuthContext);
-    const navigate = useNavigate();
-    const location = useLocation();
-    const [activeRoute, setActiveRoute] = useState(location.pathname);
-    const isModerator = useModerator();
-    const isAdmin = useAdmin();
     
-    useEffect(() => {
-        if(isAdmin[0]) {
-            navigate('/dashboard/adminHome');
-        } else if(isModerator[0]) {
-            navigate('/dashboard/productReviewPage')
-        } else {
-            navigate('/dashboard/userProfile')
-        }
-    }, [])
-
-    useEffect(() => {        
-        setActiveRoute(location.pathname);
-    }, [location])
-
-    // console.log(activeRoute)
-    // console.log(isAdmin[0], isModerator[0])
-
-    const dashboardLogOut = () => {
-        logOut()
-        navigate('/');
-    };
+    const { 
+        activeRoute, 
+        dashboardLogOut, 
+        user, 
+        isAdmin, 
+        isModerator 
+    } = useDashboard();
 
     return (
         <>
@@ -53,7 +29,6 @@ const Dashboard = () => {
                 <Outlet/>
             </div>
         </div>
-        <Footer></Footer>
         </>
     );
 };

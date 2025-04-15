d44a87888f358376a04c5a822918da4f599163bc
khalid09dev <mdkhalidhossen@hotmail.com>
2025-04-15 17:29:57 +0600

    style: formatted jsx code in a good manner

--- src/components/UserPaymentSuccess/PaymentSuccess.jsx
+++ src/components/UserPaymentSuccess/PaymentSuccess.jsx
@@ -20,9 +20,15 @@ const PaymentSuccess = () => {
     return (
         <div className="pb-20 pt-32 border bg-[#F5F8F9] dark:bg-black">
         <div className="border md:w-2/5 w-4/5 mx-auto pt-5 pb-5 rounded-2xl px-10 bg-white dark:bg-gray-200">
-            <div className="bg-[#23A26D1F] p-3 rounded-full w-[54px] mx-auto"><IoMdCheckmark className="text-white bg-[#23A26D] rounded-full text-3xl p-1"/></div>
-            <PaymentDetailsRow label="Transaction ID" value={tran_id}/>
-            <PaymentDetailsRow label="Payment Time" value={tran_date}/>
+            <div className="bg-[#23A26D1F] p-3 rounded-full w-[54px] mx-auto">
+                <IoMdCheckmark className="text-white bg-[#23A26D] rounded-full text-3xl p-1"/>
+            </div>
+            <PaymentDetailsRow 
+                label="Transaction ID" 
+                value={tran_id}
+            />
+            <PaymentDetailsRow 
+            label="Payment Time" value={tran_date}/>
             <PaymentDetailsRow 
                 label="Payment Method" 
                 value={card_issuer === 'undefined' ? 'Null' : card_issuer}

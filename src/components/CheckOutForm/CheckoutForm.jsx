83c62e67242d85384f019164618c182c56fcc953
khalid09dev <mdkhalidhossen@hotmail.com>
2025-04-15 16:20:47 +0600

    refactor: moved logic & states into custom hook and formatted code style

--- src/components/CheckOutForm/CheckoutForm.jsx
+++ src/components/CheckOutForm/CheckoutForm.jsx
@@ -1,28 +1,27 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import { useEffect, useState } from "react";
-import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { useState } from "react";
 import useStripePayment from "../../hooks/useStripePayment";
+import useCreatePaymentIntent from "../../hooks/useCreatePaymentIntent";
 
 const CheckoutForm = () => {
     const [error, setError] = useState('');
-    const [clientSecret, setClientSecret] = useState('');
     const [transactionId, setTransactionId] = useState('');
     const [loading, setLoading] = useState(false);
     const stripe = useStripe();
     const elements = useElements();
-    const axiosSecure = useAxiosSecure();
-    const totalPrice = 50;
+    
+    const { clientSecret, totalPrice } = useCreatePaymentIntent();
 
-    useEffect(() => {
-        axiosSecure.post('/payments/create-payment-intent', {price: totalPrice})
-        .then((res) => {
-            // console.log(res.data.clientSecret);
-            setClientSecret(res.data.clientSecret)
-        })
-        // console.log(totalPrice)
-    }, [axiosSecure, totalPrice])
-
-    const handleSubmit = useStripePayment(stripe, setError, setTransactionId, elements, CardElement, clientSecret, totalPrice, setLoading);
+    const handleSubmit = useStripePayment(
+        stripe, 
+        setError, 
+        setTransactionId, 
+        elements, 
+        CardElement, 
+        clientSecret, 
+        totalPrice, 
+        setLoading
+    );
 
     return (
         <div>
@@ -44,7 +43,11 @@ const CheckoutForm = () => {
             }}
             />
 
-            <button className='btn btn-sm btn-primary py-1 px-4 rounded my-3 bg-[#686EF8]' type='submit' disabled={!stripe || !clientSecret}>{loading ? 'Subscribing...' : 'Subscribe'}</button>
+            <button 
+                className='btn btn-sm btn-primary py-1 px-4 rounded my-3 bg-[#686EF8]' 
+                type='submit' 
+                disabled={!stripe || !clientSecret}>{loading ? 'Subscribing...' : 'Subscribe'}
+            </button>
             <p className="text-red-600">{error}</p>
             {transactionId && <p className="text-green-600">Your Transaction Id:  {transactionId}</p>}
             </form>

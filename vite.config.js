e6c40cc13013ab0fe5bf3cbd7e93b22400a92697
khalid09dev <mdkhalidhossen@hotmail.com>
2025-05-02 10:32:19 +0600

    chore: removed vite-rollup bundle size visualizer

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import tsconfigPaths from "vite-tsconfig-paths"
-import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
-    visualizer({
-      open: true,
-      gzipSize: true,
-      brotliSize: true
-    })
   ],
   resolve: {
     alias: {
